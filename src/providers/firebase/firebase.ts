ae4022d28558f01f7ee076b1889fb7c19c52c7c5
NahuelPatane <nahupatane@gmail.com>
2017-11-28 16:58:19 -0300

    agregar img & borrar

--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import * as firebase from 'firebase';
-import { AngularFireObject } from 'angularfire2/database/interfaces';
 
 
 /*
@@ -19,47 +18,45 @@ export class FirebaseProvider {
   constructor(public http: HttpClient, public fb: AngularFireDatabase) {
   }
 
-  getComida() {
+  getComida(){
     return this.fb.list('comidas').valueChanges();
   }
 
-  getComidaByID(id) {
-    return this.fb.object('comidas/' + id).valueChanges();
+  getComidaByID(id){
+    return this.fb.object('comidas/'+id).valueChanges();
   }
 
-  borrar(id) {
-    const ref = 'comidas/' + id
-    var obj = this.fb.object(ref);
-    obj.remove()
+  borrar(id){
+    return this.fb.object('comidas/'+ id).remove();
   }
 
-  agregarPlato(nombre, proteinas, ch, grasas, calorias, img: File) {
-    const plato = this.fb.database.ref('comidas').push();
-    const storageRef = this.storage.ref()
-    var task = storageRef.child('IMAGE/' + plato.key).put(img)
-    task.on(firebase.storage.TaskEvent.STATE_CHANGED,
-      (snap) => {
-        alert("Subiendo")
-      }, error => {
-        plato.remove()
-        alert(error)
-      }, () => {
-        var data = {
-          "image": task.snapshot.downloadURL,
-          "calorias": calorias,
-          "ch": ch,
-          "grasas": grasas,
-          "id": plato.key,
-          "nombre": nombre,
-          "proteinas": proteinas
-        }
-        plato.set(data)
+agregarPlato(nombre, proteinas, ch, grasas, calorias, img: File){
+  const plato = this.fb.database.ref('comidas').push();
+  const storageRef = this.storage.ref()
+  var task = storageRef.child('IMAGE/'+plato.key).put(img)
+  task.on(firebase.storage.TaskEvent.STATE_CHANGED, 
+  (snap) => {
+    alert("Subiendo")
+  }, error => {
+    plato.remove()
+    alert(error)
+  }, () => {
+    var data = {
+      "image": task.snapshot.downloadURL,
+      "calorias" : calorias,
+      "ch" : ch,
+      "grasas" : grasas,
+      "id" : plato.key,
+      "nombre" : nombre,
+      "proteinas" : proteinas
+    }
+    plato.set(data)
+    
+  })
 
-      })
+  
 
-
-
-  }
+}
 
 
 }
